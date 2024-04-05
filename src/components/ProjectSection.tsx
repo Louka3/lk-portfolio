dc2c6b39ee05387b9cf525b6933bad7780330d0b
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-04-05 17:10:11 -0400

    replaced hard coded projects with ProjectCard components being passed the relevant information

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,5 +1,6 @@
 'use server';
 import Link from 'next/link';
+import { ProjectCard } from '../components/ProjectCard';
 
 export const ProjectSection = (): JSX.Element => {
   return (
@@ -9,117 +10,35 @@ export const ProjectSection = (): JSX.Element => {
         id="card-container"
         className="grid grid-cols-2 gap-5 font-merriweather"
       >
-        <a
-          href="https://github.com/oslabs-beta/React-Labyrinth/"
-          target="_blank"
-        >
-          <div className="max-w-xxl bg-reactLab bg-cover bg-center rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="hover:opacity-80 min-h-60 bg-white opacity-85 h-full w-full p-6 border-2 rounded-lg border-solid border-black">
-              <svg
-                width="46"
-                height="48"
-                viewBox="0 0 98 96"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                  fill="#24292f"
-                />
-              </svg>
-              <h5 className="mb-2 text-3xl font-semibold tracking-tight text-black">
-                React Labyrinth
-              </h5>
-              <p className="mb-3 font-medium text-xl text-black">
-                A VS Code Extension that generates a hierarchical tree of React
+        <ProjectCard
+          name="React Labyrinth"
+          url="https://github.com/oslabs-beta/React-Labyrinth/"
+          description="A VS Code Extension that generates a hierarchical tree of React
                 components and identifies the component type with a single file
-                upload.
-              </p>
-            </div>
-          </div>
-        </a>
-        <a
-          href="https://github.com/Articon-scratch-project/Articon"
-          target="_blank"
-        >
-          <div className="max-w-xxl bg-articon bg-cover bg-center rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="hover:opacity-70  min-h-60 bg-white opacity-80 h-full w-full border-2 border-black p-6 rounded-lg">
-              <svg
-                width="46"
-                height="48"
-                viewBox="0 0 98 96"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                  fill="#24292f"
-                />
-              </svg>
-              <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 ">
-                Articon
-              </h5>
-              <p className="mb-3 font-medium text-xl text-black ">
-                An application that allows you to find famous artworks to use as
-                reaction images for various emotions and situations.
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="https://github.com/IterationDreamTeam/Co-Sync" target="_blank">
-          <div className="max-w-xxl bg-coSync bg-cover bg-center rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="hover:opacity-80 min-h-60 bg-white opacity-85 h-full w-full p-6 border-2 border-black rounded-lg">
-              <svg
-                width="46"
-                height="48"
-                viewBox="0 0 98 96"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                  fill="#24292f"
-                />
-              </svg>
-              <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900">
-                Co-Sync
-              </h5>
-              <p className="mb-3 font-medium text-xl text-black ">
-                Co-Sync is a SCRUM app. It&apos;s main focus is to help teams
-                manage their projects and tasks.
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="https://github.com/Louka3/personal-bookshelf" target="_blank">
-          <div className="max-w-xxl bg-personalBookshelf bg-cover bg-center rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className="hover:opacity-80 min-h-60 bg-white opacity-85 h-full w-full p-6 border-2 border-black rounded-lg">
-              <svg
-                width="46"
-                height="48"
-                viewBox="0 0 98 96"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                  fill="#24292f"
-                />
-              </svg>
-              <h5 className="mb-2 text-3xl font-semibold tracking-tight text-gray-900">
-                Personal Bookshelf
-              </h5>
-              <p className="mb-3 font-medium text-xl text-black">
-                A personal digital library to help users keep track of books
-                they&apos;ve read, enjoyed, or dropped before finishing.
-              </p>
-            </div>
-          </div>
-        </a>
+                upload."
+          imgName="reactLab"
+        />
+        <ProjectCard
+          name="Articon"
+          url="https://github.com/Articon-scratch-project/Articon"
+          description="An application that allows you to find famous artworks to use as
+          reaction images for various emotions and situations."
+          imgName="articon"
+        />
+        <ProjectCard
+          name="Co-Sync"
+          url="https://github.com/IterationDreamTeam/Co-Sync"
+          description="Co-Sync is a SCRUM app. It's main focus is to help teams
+          manage their projects and tasks."
+          imgName="coSync"
+        />
+        <ProjectCard
+          name="Personal Bookshelf"
+          url="https://github.com/Louka3/personal-bookshelf"
+          description="A personal digital library to help users keep track of books
+          they've read, enjoyed, or dropped before finishing."
+          imgName="personalBookshelf"
+        />
       </div>
     </div>
   );
