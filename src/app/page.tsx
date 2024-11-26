966011c4e8e8d6d5e4ba79be3a5b784c5474e3ca
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-11-26 01:12:46 -0500

    commented out bottom footer. seems a little junior. changed background to white. removed dark mode for now. Made header remain on the screen the whole time for easier navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <AboutSection />
         <ProjectSection />
         <ContactSection />
-        <Footer />
+        {/* <Footer /> */}
       </main>
     </>
   );
