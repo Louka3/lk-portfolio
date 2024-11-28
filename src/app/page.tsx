1effe9f6a447f7561b2416a3014d885a27204e3a
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-11-27 22:03:20 -0500

    import paths changed to reflect changes in components placement in directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 // import Link from 'next/link';
-import { Navbar } from '../components/Navbar';
-import { AboutSection } from '../components/AboutSection';
-import { ProjectSection } from '../components/ProjectSection';
-import { ContactSection } from '../components/ContactSection';
-import { Footer } from '../components/Footer';
+import { Navbar } from '../components/Home/Navbar';
+import { AboutSection } from '../components/Home/AboutSection';
+import { ProjectSection } from '../components/Project/ProjectSection';
+import { ContactSection } from '../components/Home/ContactSection';
+import { Footer } from '../components/Home/Footer';
 
 export default function Home() {
   return (
