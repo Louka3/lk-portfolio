815c4ca50df91f9744c021d58f735dd0c69437c5
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-12-02 02:15:27 -0500

    began to add placeholder content to the project page

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,3 +1,5 @@
+import './projects.css';
+
 export default function ProjectsLayout({
   children,
 }: {
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,8 +7,29 @@ export default function Projects({
 }) {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center pt-24">
-        <h1>Project Name: {searchParams.name}</h1>
+      <main className="w-[80%] flex min-h-screen flex-col items-center pt-24">
+        <h1 className="project-headers">Project Name: {searchParams.name}</h1>
+        <p id="summary">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta,
+          dolorum veritatis ipsum est repellendus expedita itaque magnam dolore
+          nulla accusamus assumenda repellat vel atque possimus. Laboriosam
+          iusto ipsum saepe commodi?
+        </p>
+        <div id="tech-stack-container" className="flex flex-col items-center">
+          <h2 className="project-headers">Tech Stack</h2>
+          maybe put little rectangles with the tech name in them here
+        </div>
+        <div id="image-of-the-project" className="h-80 w-96 border">
+          large image of the site
+        </div>
+        <div id="project-goal">
+          <h2 className="project-headers">Goals of the project</h2>
+        </div>
+        <div className="problems-that-occured">
+          <h2 className="project-headers">
+            Development Difficulties or Issues
+          </h2>
+        </div>
       </main>
     </>
   );
